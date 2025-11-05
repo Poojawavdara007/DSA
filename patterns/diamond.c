636d18e4700af19e7f66607b624d902871738333
Poojawavdara007 <poojawavdara21@gmail.com>
2025-11-06 01:20:03 +0530

    patterns

---
+++ patterns/diamond.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+	for(int i = 0 ; i<=n ; i++){
+	    for(int j = 0 ; j<=n-i ;j++){
+	        printf(" ");
+	        
+	    }
+	    for(int k =1 ; k<=(2*i-1) ; k++){
+	        printf("*");
+	    }
+	    
+	    printf("\n");
+	} 
+	for(int i = n ; i>=0 ; i--){
+	    for(int j = 0 ; j<=n-i ;j++){
+	        printf(" ");
+	        
+	    }
+	    for(int k =1 ; k<=(2*i-1) ; k++){
+	        printf("*");
+	    }
+	    
+	    printf("\n");
+	} 
+}
+
+
+//     *
+//    ***
+//   *****
+//    ***
+//     *
